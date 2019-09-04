b633829b3e2baabf1513ec1ce72cbfeca6226586
wen <18122715123@163.com>
2019-09-04 19:05:21 +0800

    修改

--- src/view/Header/styles.js
+++ src/view/Header/styles.js
@@ -1,17 +1,21 @@
 import styled from 'styled-components';
 import size from 'styles/font';
+import { tablet } from 'styles/mediaQueries';
 
 export const HeadWrap = styled.div`
     width:100%;
-    margin-top:10px;
+    margin-top:.1rem;
     display:flex;
     justify-content:space-between;
+    ${tablet} {
+        margin:0;
+    }
 `;
 export const Wrap = styled.div``;
 
 export const LangLink = styled.a`
         color:#333;
-        margin-left:5px;
+        margin-left:.05rem;
         font-size:${size.text};
         white-space:nowrap;
         &:hover {
@@ -22,12 +26,12 @@ export const Form = styled.div`
     input {
         font-size: ${size.small};
         width:1rem;
-        margin-left:5px;
+        margin-left:.05rem;
     }
     [type='button'] {
         width:.4rem;
         outline:none;
-        border-radius:5px;
+        border-radius:.05rem;
         padding:0;
         cursor: pointer;
         &:active {
